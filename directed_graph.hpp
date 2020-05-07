de986ca396810f2e3cdef393474605174e7fded0
alimickey <ad312@pm.me>
2020-05-07 15:03:18 +1000

    fix

--- directed_graph.hpp
+++ directed_graph.hpp
@@ -35,10 +35,6 @@ class directed_graph {
 
 private:
 
-	//You will need to add some data members here
-	//to actually represent the graph internally,
-	//and keep track of whatever you need to.
-	
 	vector<vertex<T>> vertice_list; // this is not design for this assignment
 	// if you are using adj_list instead of adj_matrix, you may need something like vector < vector<vertex<T>> adj_list;
 
@@ -46,31 +42,27 @@ public:
 
 	void add_vertex(const vertex<T>&); //Adds the passed in vertex to the graph (with no edges).
 	void remove_vertex(const int&); //Removes the given vertex. Should also clear any incident edges.
-	vector<vertex<T>> get_vertices(); //Returns a vector containing all the vertices.														  // A vector cannot be considered a second_order_neighbor of itself.
+	vector<vertex<T>> get_vertices(); //Returns a vector containing all the vertices.  // A vector cannot be considered a second_order_neighbor of itself.
 
 };
 
-// Define all your methods down here (or move them up into the header, but be careful you don't double up). If you want to move this into another file, you can, but you should #include the file here.
-// Although these are just the same names copied from above, you may find a few more clues in the full method headers.
-// Note also that C++ is sensitive to the order you declare and define things in - you have to have it available before you use it.
-
 template <typename T>
-void directed_graph<T>::add_vertex(const vertex<T>& u) {
-	vertice_list.push_back(u);
+void directed_graph<T>::add_vertex(const vertex<T>& v) { //Add a vertex
+	vertice_list.push_back(v);
 }
 
 template <typename T>
-void directed_graph<T>::remove_vertex(const int& u_id) { // the argument is the vertex_id
-	for (int i = 0; i < vertice_list.size(); i++) { // check on each vertex within vertice_list, which is a vector
-		if (vertice_list[i].id == u_id) {
-			vertice_list.erase(vertice_list.begin() + i); // delete the i-th vertex from the vector
+void directed_graph<T>::remove_vertex(const int& v_id) { // Remove Vertex
+	for (int i = 0; i < vertice_list.size(); i++) { // Loop through each vertex
+		if (vertice_list[i].id == v_id) {
+			vertice_list.erase(vertice_list.begin() + i); // Delete the vertex associated with provided id
 			break;
 		}
 	}
 }
 
 template <typename T>
-vector<vertex<T>> directed_graph<T>::get_vertices() { // return the vector, whicn contains all vertices of the vector
+vector<vertex<T>> directed_graph<T>::get_vertices() { // Return all vertexes. 
 	return vertice_list;
 }
 
